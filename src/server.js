58a251cadd671428b8b9ee4853b0e30dc93bb87e
BryanVillabona <bvillabona13@gmail.com>
2025-10-27 18:38:56 -0500

    feat: :sparkles: Se crea archivo del servidor principal con sus rutas
    By @BryanVillabona

---
+++ src/server.js
@@ -0,0 +1,44 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import rateLimit from "express-rate-limit";
+import { conectarBD } from "./config/db.js";
+
+import swaggerUI from 'swagger-ui-express';
+
+// Config
+const app = express();
+app.use(express.json());
+
+// Configuración de CORS
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
+// Rate Limiter
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, 
+	max: 100, 
+	message: 'Demasiadas peticiones desde esta IP, intente de nuevo en 15 minutos.',
+});
+app.use(limiter);
+
+// Rutas
+const apiV1Router = express.Router();
+
+app.use("/api/v1", apiV1Router);
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: "My Foodie Backend activo!!!", version: "1.0.0" });
+});
+
+// Executions
+conectarBD().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log(
+      `Backend escuchando en http://${process.env.HOST_NAME}:${process.env.PORT}`
+    );
+  });
+});
