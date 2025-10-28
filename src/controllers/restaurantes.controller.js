0b3bb70b8581cea388a2fdd72daaeb3145b7edeb
BryanVillabona <bvillabona13@gmail.com>
2025-10-27 22:26:49 -0500

    feat: :sparkles: funciones controlador para admin
    By @BryanVillabona

--- src/controllers/restaurantes.controller.js
+++ src/controllers/restaurantes.controller.js
@@ -29,3 +29,44 @@ export async function httpCrearRestaurante(req, res) {
         res.status(400).json({ error: error.message }); 
     }
 }
+
+//Funciones de Admin
+
+export async function httpObtenerRestaurantesPendientes(req, res) {
+    try {
+        const restaurantes = await services.obtenerRestaurantesPendientes();
+        res.status(200).json(restaurantes);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+export async function httpAprobarRestaurante(req, res) {
+    try {
+        const { id } = req.params;
+        const result = await services.aprobarRestaurante(id);
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(404).json({ error: error.message });
+    }
+}
+
+export async function httpActualizarRestaurante(req, res) {
+    try {
+        const { id } = req.params;
+        const result = await services.actualizarRestaurante(id, req.body);
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(404).json({ error: error.message });
+    }
+}
+
+export async function httpEliminarRestaurante(req, res) {
+    try {
+        const { id } = req.params;
+        const result = await services.eliminarRestaurante(id);
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(404).json({ error: error.message });
+    }
+}
